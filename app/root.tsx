2e0a22a4baf7757a486bd70d39460248c60cca48
kevinreber <kevinreber1@gmail.com>
2023-05-07 08:51:56 -0700

    Refactored Header action item buttons

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,4 @@
 import {
-  Form,
   Link,
   Links,
   LiveReload,
@@ -7,31 +6,15 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useFetcher,
   useLoaderData,
 } from "@remix-run/react";
-import { Button, Layout, Menu, Space, Typography, theme } from "antd";
+import { Button, Layout, Menu, Space, Typography, ConfigProvider } from "antd";
 import { type LoaderArgs, json } from "@remix-run/node";
 import { authenticator } from "~/services/auth.server";
-import { DollarOutlined, GoogleOutlined } from "@ant-design/icons";
+import { DollarOutlined } from "@ant-design/icons";
 import { SocialsProvider } from "remix-auth-socials";
 import { getUserData } from "~/server/";
-
-const CONTAINER_STYLES = {
-  width: 140,
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-};
-
-const BUTTON_STYLES = {
-  padding: "8px 24px",
-  background: "#dd4b39",
-  border: "0",
-  outline: "none",
-  cursor: "pointer",
-  color: "white",
-  fontWeight: "bold",
-};
 
 export let loader = async ({ request }: LoaderArgs) => {
   const user = await authenticator.isAuthenticated(request);
@@ -44,13 +27,30 @@ export let loader = async ({ request }: LoaderArgs) => {
   return json({ data: userData });
 };
 
+const INTENT_MAP = {
+  USER_LOG_IN: "user-log-in",
+  USER_LOG_OUT: "user-log-out",
+};
+
 export default function App() {
   const loaderData = useLoaderData();
   const isLoggedIn = loaderData.data;
+  const fetcher = useFetcher();
 
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
+  const handleLogIn = () => {
+    fetcher.submit(
+      { intent: INTENT_MAP.USER_LOG_IN },
+      { method: "post", action: `/auth/${SocialsProvider.GOOGLE}` }
+    );
+  };
+
+  const handleLogOut = () => {
+    fetcher.submit(
+      { intent: INTENT_MAP.USER_LOG_OUT },
+      { method: "post", action: "/logout" }
+    );
+  };
+
   return (
     <html lang='en'>
       <head>
@@ -58,94 +58,97 @@ export default function App() {
         <Links />
       </head>
       <body style={{ margin: 0 }}>
-        <Layout>
-          <Layout.Header className='header' style={{ display: "flex" }}>
-            <Typography.Title
-              level={4}
-              style={{ color: "#fff", width: 240, margin: "auto" }}
-            >
-              AI Icon Generator
-            </Typography.Title>
-            <Menu
-              style={{ width: "100%" }}
-              theme='dark'
-              mode='horizontal'
-              items={[
-                {
-                  key: "generate",
-                  label: <Link to='/generate'>Generate Icons</Link>,
-                },
-              ]}
-            />
-            {!isLoggedIn ? (
-              <Form
-                method='post'
-                action={`/auth/${SocialsProvider.GOOGLE}`}
-                style={{ ...CONTAINER_STYLES, width: 320 }}
+        <ConfigProvider
+          theme={{
+            token: {
+              colorPrimaryBorder: "#64ffda",
+              colorBgBase: "#0a1930",
+              colorPrimary: "#64ffda",
+              colorText: "#e6f1ff",
+            },
+          }}
+        >
+          <Layout>
+            <Layout.Header className='header' style={{ display: "flex" }}>
+              <Typography.Title
+                level={4}
+                style={{ color: "#fff", width: 240, margin: "auto" }}
               >
-                <button style={BUTTON_STYLES}>
-                  <GoogleOutlined /> Login with Google
-                </button>
-              </Form>
-            ) : (
-              <Space>
-                <div
-                  style={{
-                    color: "#fff",
-                    display: "flex",
-                    alignItems: "center",
-                    width: 110,
-                  }}
-                >
-                  <DollarOutlined style={{ marginRight: 4 }} />
-                  <p style={{ display: "flex", alignItems: "center" }}>
-                    {loaderData.data?.credits} Credits
-                  </p>
-                </div>
+                AI Icon Generator
+              </Typography.Title>
+              <Menu
+                style={{ width: "100%" }}
+                theme='dark'
+                mode='horizontal'
+                items={[
+                  {
+                    key: "generate",
+                    label: <Link to='/generate'>Generate Icons</Link>,
+                  },
+                ]}
+              />
+              <div style={{ margin: "auto" }}>
+                {!isLoggedIn ? (
+                  <Button onClick={handleLogIn}>Sign In</Button>
+                ) : (
+                  <Space>
+                    <div
+                      style={{
+                        color: "#fff",
+                        display: "flex",
+                        alignItems: "center",
+                        width: 110,
+                      }}
+                    >
+                      <DollarOutlined style={{ marginRight: 4 }} />
+                      <p style={{ display: "flex", alignItems: "center" }}>
+                        {loaderData.data?.credits} Credits
+                      </p>
+                    </div>
 
-                <Button
-                  icon={<DollarOutlined />}
-                  href='/checkout'
-                  type='primary'
-                  ghost
-                >
-                  Buy Credits
-                </Button>
-                <Form method='post' action={`/logout`} style={CONTAINER_STYLES}>
-                  <button style={BUTTON_STYLES}>Logout</button>
-                </Form>
-              </Space>
-            )}
-          </Layout.Header>
-          <Layout style={{ minHeight: "96vh", width: "80%", margin: "auto" }}>
-            {/* <Layout.Sider width={200} style={{ background: colorBgContainer }}> */}
-            {/* <Menu
+                    <Button
+                      icon={<DollarOutlined />}
+                      href='/checkout'
+                      type='primary'
+                      ghost
+                    >
+                      Buy Credits
+                    </Button>
+
+                    <Button onClick={handleLogOut}>Logout</Button>
+                  </Space>
+                )}
+              </div>
+            </Layout.Header>
+            <Layout style={{ minHeight: "96vh", width: "80%", margin: "auto" }}>
+              {/* <Layout.Sider width={200} style={{ background: colorBgContainer }}> */}
+              {/* <Menu
                 mode='inline'
                 defaultSelectedKeys={["1"]}
                 defaultOpenKeys={["sub1"]}
                 style={{ height: "100%", borderRight: 0 }}
                 items={[{ key: "generate", label: "Generate" }]}
               /> */}
-            {/* </Layout.Sider> */}
-            <Layout>
-              {/* <Breadcrumb style={{ margin: '16px 0' }}>
+              {/* </Layout.Sider> */}
+              <Layout>
+                {/* <Breadcrumb style={{ margin: '16px 0' }}>
             <Breadcrumb.Item>Home</Breadcrumb.Item>
             <Breadcrumb.Item>List</Breadcrumb.Item>
             <Breadcrumb.Item>App</Breadcrumb.Item>
           </Breadcrumb> */}
-              <Layout.Content
-                style={{
-                  padding: 24,
-                  margin: 0,
-                  minHeight: 280,
-                  background: colorBgContainer,
-                }}
-              >
-                <Outlet />
-              </Layout.Content>
+                <Layout.Content
+                  style={{
+                    padding: 24,
+                    margin: 0,
+                    minHeight: 280,
+                  }}
+                >
+                  <Outlet />
+                </Layout.Content>
+              </Layout>
             </Layout>
           </Layout>
-        </Layout>
+        </ConfigProvider>
 
         <ScrollRestoration />
         <Scripts />
