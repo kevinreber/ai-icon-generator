bf572bf0daa0940ad466075ab624bbdd0a1553a8
kevinreber <kevinreber1@gmail.com>
2023-06-15 21:15:00 -0700

    added stripe webhook error handler and comments

--- app/services/stripe.server.ts
+++ app/services/stripe.server.ts
@@ -1,24 +1,26 @@
 import Stripe from "stripe";
+import { json } from "@remix-run/node";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2022-11-15",
 });
 
-export const stripeCheckout = async ({
-  userId,
-}: {
-  userId: string;
-}): Promise<string> => {
-  const session = await stripe.checkout.sessions.create({
-    success_url: `${process.env.ORIGIN}/generate`!,
-    cancel_url: `${process.env.ORIGIN}/generate`!,
-    line_items: [{ price: process.env.STRIPE_CREDITS_PRICE_ID, quantity: 1 }],
-    mode: "payment",
-    metadata: {
-      userId: userId,
-    },
-    payment_method_types: ["card", "us_bank_account"],
-  });
+export const stripeCheckout = async ({ userId }: { userId: string }) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      success_url: `${process.env.ORIGIN}/generate`!,
+      cancel_url: `${process.env.ORIGIN}/generate`!,
+      line_items: [{ price: process.env.STRIPE_CREDITS_PRICE_ID, quantity: 1 }],
+      mode: "payment",
+      metadata: {
+        userId: userId,
+      },
+      payment_method_types: ["card", "us_bank_account"],
+    });
 
-  return session.url!;
+    return session.url!;
+  } catch (error: any) {
+    console.error(error)
+    throw json({ errors: [{ message: error.message }] }, 400);
+  }
 };
