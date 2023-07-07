fdec729718e13900604ac071453f791b71c6b990
kevinreber <kevinreber1@gmail.com>
2023-07-07 12:03:19 -0700

    Updated `logout` function

--- app/routes/logout.ts
+++ app/routes/logout.ts
@@ -2,5 +2,22 @@ import { type ActionArgs } from "@remix-run/node";
 import { authenticator } from "~/services/auth.server";
 
 export const action = async ({ request }: ActionArgs) => {
-  await authenticator.logout(request, { redirectTo: "/" });
+  try {
+    const data = await authenticator.logout(request, { redirectTo: "/" });
+
+    return {
+      success: true,
+      message: "Successfully logged out",
+      data,
+    };
+  } catch (error) {
+    const message = "Error while attempting to log out, please try again";
+    console.error(error);
+    return {
+      success: false,
+      message,
+      data: {},
+      error,
+    };
+  }
 };
