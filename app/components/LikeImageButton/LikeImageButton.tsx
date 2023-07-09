1b4e164ed2f4e422154dc32439895904d0d53816
kevinreber <kevinreber1@gmail.com>
2023-07-08 20:17:14 -0700

    Created `LikeImageButton` component. This will handle all Like/Unlike actions of an image

---
+++ app/components/LikeImageButton/LikeImageButton.tsx
@@ -0,0 +1,49 @@
+import { HeartOutlined, HeartTwoTone } from "@ant-design/icons";
+import { Button } from "antd";
+import type { ImageType } from "~/types";
+import { useRemixFetcher } from "~/hooks";
+import { useLoaderData } from "@remix-run/react";
+
+const LikeImageButton = ({ imageData }: { imageData: ImageType }) => {
+  const loaderData = useLoaderData();
+  const userId = loaderData.user?.id || undefined;
+  const { fetcher, isLoadingFetcher } = useRemixFetcher();
+
+  const handleLikeImage = () => {
+    // User must be logged in to Like an Image
+    if (!userId) {
+      return;
+    }
+
+    fetcher.submit(
+      { intent: "image-toggle-like" },
+      {
+        method: "post",
+        action: `api/image/${imageData.id}/like?index`,
+      }
+    );
+  };
+
+  const userLikesImage = imageData.likes.some((like) => like.userId === userId);
+
+  const buttonIcon = userLikesImage ? (
+    <HeartTwoTone twoToneColor='#eb2f96' />
+  ) : (
+    <HeartOutlined style={{ color: "#eb2f96" }} />
+  );
+
+  return (
+    <Button
+      size='small'
+      style={{ border: "none" }}
+      icon={buttonIcon}
+      onClick={handleLikeImage}
+      loading={isLoadingFetcher}
+      disabled={!userId}
+    >
+      <span>{imageData.likes.length > 0 && imageData.likes.length}</span>
+    </Button>
+  );
+};
+
+export default LikeImageButton;
--- app/components/index.ts
+++ app/components/index.ts
@@ -1,2 +1,3 @@
 export * from "./UserAvatar";
 export * from "./CopyToClipboardButton/";
+export * from "./LikeImageButton/";
