9cc09de8ab4f257d17049c6d9a32caa0a1f647d3
kevinreber <kevinreber1@gmail.com>
2023-08-06 13:35:40 -0700

    Updated mock data

---
+++ app/__mocks__/getUserData.mock.ts
@@ -0,0 +1,46 @@
+export const MOCK_GET_USER_DATA = [
+  {
+    id: 101712902930004640000,
+    name: "Narinder Maurya",
+    email: "mauryanarinder2002@gmail.com",
+    image:
+      "https://lh3.googleusercontent.com/a/AAcHTtfrERhIEAcmWx6whZIvw7lUVMLJ9UCRZAgjmoudM-f8TnE=s96-c",
+    credits: 4,
+    createdAt: "2023-07-03 11:25:40.666",
+    username: "Narinder Maurya",
+    updatedAt: "2023-07-03 18:25:40",
+  },
+  {
+    id: 101004161584919920000,
+    name: "sobhan sajjadi",
+    email: "pesareirani100@gmail.com",
+    image:
+      "https://lh3.googleusercontent.com/a/AAcHTte6_CcqMjVPOA4XSiU12sON4AeR85u0c2-6zVjk2ZtoLV8=s96-c",
+    credits: 0,
+    createdAt: "2023-08-01 16:18:07.454",
+    username: "sobhan sajjadi",
+    updatedAt: "2023-08-01 16:19:18.945",
+  },
+  {
+    id: 100468795116526200000,
+    name: "Kevin Reber",
+    email: "kevinreber1@gmail.com",
+    image:
+      "https://lh3.googleusercontent.com/a/AAcHTtc0pBxRaHU5ww7xPxRsmpa7DxKkY6_-WU05FXTsnw=s96-c",
+    credits: 540,
+    createdAt: "2023-04-19 03:28:42",
+    username: "reebeezie",
+    updatedAt: "2023-08-01 17:28:28.376",
+  },
+  {
+    id: 115925789600352650000,
+    name: "Kevin Reber",
+    email: "kevinreber@berkeley.edu",
+    image:
+      "https://lh3.googleusercontent.com/a/AAcHTtcRd5jiixdL8cznSQ9MLiuC8L5q45955A0_KbYBOdRJ=s96-c",
+    credits: 0,
+    createdAt: "2023-07-01 04:09:35.199",
+    username: "RBRFVR1",
+    updatedAt: "2023-07-10 04:42:30.079",
+  },
+];
