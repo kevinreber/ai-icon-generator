d934c8e41122a76bf3aa04163c12aadee7cdc737
kevinreber <kevinreber1@gmail.com>
2023-08-06 21:00:50 -0700

    debugging styles

--- app/root.tsx
+++ app/root.tsx
@@ -8,7 +8,7 @@ import {
   useFetcher,
   useLoaderData,
 } from "@remix-run/react";
-import { Button, Layout, Space, Typography } from "antd";
+import { Button, Layout, Space, Typography, ConfigProvider } from "antd";
 import { type LoaderArgs, json } from "@remix-run/node";
 import { authenticator } from "~/services/auth.server";
 import { SocialsProvider } from "remix-auth-socials";
@@ -17,12 +17,11 @@ import { UserAvatar } from "./components";
 import { UserContext } from "~/context";
 
 // CSS
-// import globalStyles from "~/css/global.css";
-import "~/css/global.css";
+import globalStyles from "~/css/global.css";
 
-// export function links() {
-//   return [{ rel: "stylesheet", href: globalStyles }];
-// }
+export function links() {
+  return [{ rel: "stylesheet", href: globalStyles }];
+}
 
 export let loader = async ({ request }: LoaderArgs) => {
   const user = await authenticator.isAuthenticated(request);
@@ -68,85 +67,87 @@ export default function App() {
       </head>
       <body style={{ margin: 0 }}>
         <UserContext.Provider value={userData}>
-          {/* <ConfigProvider
-          theme={{
-            hashed: false,
-            token: {
-              colorPrimaryBorder: "#64ffda",
-              colorBgBase: "#0a1930",
-              colorPrimary: "#64ffda",
-              colorText: "#e6f1ff",
-              colorTextSecondary: "#ccd7f5",
-              colorTextPlaceholder: "#495670",
-              colorTextDisabled: "#495670",
-            },
-          }}
-        > */}
-          <Layout>
-            <Layout.Header
-              className='header'
-              style={{ display: "flex", justifyContent: "space-between" }}
-            >
-              <Typography.Link
-                href='/'
-                style={{
-                  color: "#e6f1ff",
-                  width: 240,
-                  margin: "auto 0",
-                  fontSize: 18,
-                  fontWeight: 600,
-                }}
+          <ConfigProvider
+          // theme={{
+          //   hashed: false,
+          //   token: {
+          //     colorPrimaryBorder: "#64ffda",
+          //     colorBgBase: "#0a1930",
+          //     colorPrimary: "#64ffda",
+          //     colorText: "#e6f1ff",
+          //     colorTextSecondary: "#ccd7f5",
+          //     colorTextPlaceholder: "#495670",
+          //     colorTextDisabled: "#495670",
+          //   },
+          // }}
+          >
+            <Layout>
+              <Layout.Header
+                className='header'
+                style={{ display: "flex", justifyContent: "space-between" }}
               >
-                AI Image Generator
-              </Typography.Link>
-
-              <Space>
-                <Button
-                  type='link'
-                  href='/explore'
-                  style={{ width: 64, color: "#fff" }}
-                >
-                  Explore
-                </Button>
-                {!isLoggedIn ? (
-                  <Button onClick={handleLogIn}>Sign In</Button>
-                ) : (
-                  <>
-                    <Button
-                      href='/collections'
-                      type='link'
-                      style={{ color: "#fff" }}
-                    >
-                      Collections
-                    </Button>
-                    <Button
-                      href='/create'
-                      type='primary'
-                      ghost
-                      style={{ width: 100, marginRight: 10 }}
-                    >
-                      Create
-                    </Button>
-                    <UserAvatar />
-                  </>
-                )}
-              </Space>
-            </Layout.Header>
-            <Layout style={{ minHeight: "96vh", width: "80%", margin: "auto" }}>
-              <Layout>
-                <Layout.Content
+                <Typography.Link
+                  href='/'
                   style={{
-                    padding: 24,
-                    margin: 0,
-                    minHeight: 280,
+                    color: "#e6f1ff",
+                    width: 240,
+                    margin: "auto 0",
+                    fontSize: 18,
+                    fontWeight: 600,
                   }}
                 >
-                  <Outlet />
-                </Layout.Content>
+                  AI Image Generator
+                </Typography.Link>
+
+                <Space>
+                  <Button
+                    type='link'
+                    href='/explore'
+                    style={{ width: 64, color: "#fff" }}
+                  >
+                    Explore
+                  </Button>
+                  {!isLoggedIn ? (
+                    <Button onClick={handleLogIn}>Sign In</Button>
+                  ) : (
+                    <>
+                      <Button
+                        href='/collections'
+                        type='link'
+                        style={{ color: "#fff" }}
+                      >
+                        Collections
+                      </Button>
+                      <Button
+                        href='/create'
+                        type='primary'
+                        ghost
+                        style={{ width: 100, marginRight: 10 }}
+                      >
+                        Create
+                      </Button>
+                      <UserAvatar />
+                    </>
+                  )}
+                </Space>
+              </Layout.Header>
+              <Layout
+                style={{ minHeight: "96vh", width: "80%", margin: "auto" }}
+              >
+                <Layout>
+                  <Layout.Content
+                    style={{
+                      padding: 24,
+                      margin: 0,
+                      minHeight: 280,
+                    }}
+                  >
+                    <Outlet />
+                  </Layout.Content>
+                </Layout>
               </Layout>
             </Layout>
-          </Layout>
-          {/* </ConfigProvider> */}
+          </ConfigProvider>
         </UserContext.Provider>
 
         <ScrollRestoration />
