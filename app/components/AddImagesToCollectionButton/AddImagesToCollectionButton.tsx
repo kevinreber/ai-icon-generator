18f0315f18835688b4c6b41474ac19397587d0cc
kevinreber <kevinreber1@gmail.com>
2023-11-04 22:01:25 -0700

    Added new feature to add multiple images to a collection

---
+++ app/components/AddImagesToCollectionButton/AddImagesToCollectionButton.tsx
@@ -0,0 +1,127 @@
+import React from "react";
+import { BookOutlined } from "@ant-design/icons";
+import { Button, Checkbox, Popover, Space, Typography } from "antd";
+import type { ImageType } from "~/types";
+import { useRemixFetcher } from "~/hooks";
+import { UserContext } from "~/context";
+import { CreateCollectionButton } from "~/components";
+import { useActionData, useNavigation } from "@remix-run/react";
+import { CreateImagePageActionData } from "~/routes/create._index";
+
+const getImageIds = (collections: any[]) => {
+  const imageIds: string[] = [];
+
+  collections.forEach((collection) => {
+    collection.images.forEach((image: { imageId: string }) => {
+      imageIds.push(image.imageId);
+    });
+  });
+
+  return imageIds;
+};
+
+const AddImagesToCollectionButton = ({ images }: { images: ImageType[] }) => {
+  const userData = React.useContext(UserContext);
+  const userId = userData?.id || undefined;
+
+  const actionData = useActionData() as CreateImagePageActionData;
+  // const navigation = useNavigation();
+  // const isLoadingData = navigation.state !== "idle";
+
+  console.log(actionData);
+
+  const imagesGenerated = Boolean(actionData && actionData.images);
+  const imagesIds = images.map((image) => image.id);
+
+  const { fetcher, isLoadingFetcher } = useRemixFetcher({
+    // onSuccess: (response) => {
+    //   const successMessage =
+    //     response.data.message || "Successfully added Image to Collection";
+    //   notification.success({ message: successMessage });
+    // },
+    // onError: (error) => {
+    //   const errorMessage =
+    //     error.data.message || "Error adding Image to Collection";
+    //   console.error(error);
+    //   notification.error({ message: errorMessage });
+    // },
+  });
+  const [showUserCollectionsPopover, setShowUserCollectionsPopover] =
+    React.useState(false);
+
+  const userCollections = userData.collections || [];
+  const userCollectionImages = getImageIds(userCollections);
+
+  const toggleUserCollectionsPopover = () => {
+    setShowUserCollectionsPopover(!showUserCollectionsPopover);
+  };
+
+  const handleAddImagesToCollection = (event: any) => {
+    const collectionId = event.target.value;
+    // User must be logged in to add an Image to a Collection
+    if (!userId) {
+      return;
+    }
+
+    fetcher.submit(
+      {
+        intent: "_add-image-to-collection",
+        body: JSON.stringify({ images: imagesIds }),
+      },
+      {
+        method: "POST",
+        action: `/api/collections/${collectionId}/images?index`,
+      },
+    );
+    toggleUserCollectionsPopover();
+  };
+
+  return (
+    <>
+      <Popover
+        open={showUserCollectionsPopover}
+        title={
+          <>
+            {userCollections.length ? (
+              <Space direction="vertical">
+                <CreateCollectionButton />
+                {userCollections.map((collection) => {
+                  const isCollectionChecked = userCollectionImages.some(
+                    (imageId) => imagesIds.includes(imageId),
+                  );
+
+                  return (
+                    <Checkbox
+                      key={collection.id}
+                      value={collection.id}
+                      onClick={handleAddImagesToCollection}
+                      checked={isCollectionChecked}
+                      style={{ padding: 4 }}
+                    >
+                      {collection.title}
+                    </Checkbox>
+                  );
+                })}
+              </Space>
+            ) : (
+              <Typography.Text italic type="secondary">
+                No Collections
+              </Typography.Text>
+            )}
+          </>
+        }
+      >
+        <Button
+          icon={<BookOutlined />}
+          onClick={toggleUserCollectionsPopover}
+          loading={isLoadingFetcher}
+          disabled={!userId || !imagesGenerated}
+        >
+          Add images to Collection
+        </Button>
+      </Popover>
+    </>
+  );
+};
+
+export default AddImagesToCollectionButton;
--- app/routes/api.collections.$collectionId.images._index.ts
+++ app/routes/api.collections.$collectionId.images._index.ts
@@ -1,6 +1,17 @@
-import { type LoaderFunctionArgs, json, redirect } from "@remix-run/node";
-import { getCollectionData } from "~/server";
+import {
+  type LoaderFunctionArgs,
+  json,
+  redirect,
+  type ActionFunctionArgs,
+} from "@remix-run/node";
+import {
+  addImageToCollection,
+  removeImageFromCollection,
+  getCollectionData,
+} from "~/server";
+import { getSession } from "~/services";
 import { authenticator } from "~/services/auth.server";
+import { prisma } from "~/services/prisma.server";
 
 export const loader = async ({ request, params }: LoaderFunctionArgs) => {
   await authenticator.isAuthenticated(request, {
@@ -15,3 +26,57 @@ export const loader = async ({ request, params }: LoaderFunctionArgs) => {
 
   return json({ data: collectionData });
 };
+
+export async function action({ request, params }: ActionFunctionArgs) {
+  const session = await getSession(request.headers.get("Cookie"));
+  const googleSessionData = (await session.get("_session")) || undefined;
+  const userId = googleSessionData.id;
+  const collectionId = params.collectionId || "";
+
+  if (!userId) {
+    throw new Error("Missing User ID: Must be logged in to Delete Collection");
+  }
+
+  switch (request.method.toUpperCase()) {
+    case "POST": {
+      const formData = await request.formData();
+      const payload = formData.get("body");
+      const formattedPayload = await JSON.parse(payload as string);
+      const images: string[] = formattedPayload.images;
+      const dataResponse: any = [];
+
+      await Promise.all(
+        images.map(async (imageId) => {
+          let data;
+          const collectionHasImage = await prisma.collectionHasImage.findMany({
+            where: {
+              collectionId,
+              imageId,
+            },
+          });
+
+          // If Collection already has Image, remove Image from Collection
+          if (collectionHasImage.length > 0) {
+            data = await removeImageFromCollection({
+              collectionId,
+              imageId,
+            });
+          } else {
+            data = await addImageToCollection({
+              collectionId,
+              imageId,
+            });
+          }
+          dataResponse.push(data);
+        }),
+      ).catch((error) => {
+        console.error(error);
+      });
+
+      return dataResponse;
+    }
+    default: {
+      return {};
+    }
+  }
+}
