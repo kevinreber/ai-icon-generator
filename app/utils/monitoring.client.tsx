3c4f7a9c2c8fba4f0262cd27805bc482135bcdc4
Kevin Reber <kreber@linkedin.com>
2024-01-10 23:34:14 -0800

    typo

--- app/utils/monitoring.client.tsx
+++ app/utils/monitoring.client.tsx
@@ -1,10 +1,4 @@
 import * as Sentry from "@sentry/remix";
-/**
- * By default, Remix will handle hydrating your app on the client for you.
- * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
- * For more information, see https://remix.run/docs/en/main/file-conventions/entry.client
- */
-
 import { useLocation, useMatches } from "@remix-run/react";
 import { useEffect } from "react";
 
