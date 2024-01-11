b69c1e66f8cd6a2dc9318bf9c7be0802f326f7ab
Kevin Reber <kreber@linkedin.com>
2024-01-10 23:18:55 -0800

    moved sentry logic

---
+++ app/utils/monitoring.client.tsx
@@ -0,0 +1,29 @@
+import * as Sentry from "@sentry/remix";
+/**
+ * By default, Remix will handle hydrating your app on the client for you.
+ * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
+ * For more information, see https://remix.run/docs/en/main/file-conventions/entry.client
+ */
+
+import { useLocation, useMatches } from "@remix-run/react";
+import { useEffect } from "react";
+
+export function init() {
+  Sentry.init({
+    dsn: "https://b1d58bbbf18e16501f1b26b6de2fa6a7@o4506551058759680.ingest.sentry.io/4506551062757376",
+    tracesSampleRate: 1,
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1,
+
+    integrations: [
+      new Sentry.BrowserTracing({
+        routingInstrumentation: Sentry.remixRouterInstrumentation(
+          useEffect,
+          useLocation,
+          useMatches,
+        ),
+      }),
+      new Sentry.Replay(),
+    ],
+  });
+}
